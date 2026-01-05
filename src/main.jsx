8f8ce86993d2f759a6933c4daf61df08d2f27450
Pabloo626 <pablo.andres99.moreno@gmail.com>
2026-01-04 22:25:11 -0300

    fix pwa for github pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import "./styles.css";
+import { registerSW } from "virtual:pwa-register";
 
+registerSW({ immediate: true });
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
